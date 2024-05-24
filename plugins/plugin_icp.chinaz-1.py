543bcc41c706fb35fcfaef547369fcc31850c8b3
test <test>
2024-05-25 00:44:34 +0800

    update by 5.25

--- plugins/plugin_icp.chinaz-1.py
+++ plugins/plugin_icp.chinaz-1.py
@@ -15,32 +15,36 @@ def info():
 def execute(target):
     headers = {
         'Host': 'icp.chinaz.com',
-        # 'Content-Length': '22',
-        'Sec-Ch-Ua': '"Not A(Brand";v="24", "Chromium";v="110"',
+        # 'Content-Length': '20',
+        'Sec-Ch-Ua': '"Not(A:Brand";v="24", "Chromium";v="122"',
+        'Rd': '521',
+        'Ts': '1716568712164',
         'Sec-Ch-Ua-Mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.178 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.112 Safari/537.36',
         'Content-Type': 'application/json',
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'X-Requested-With': 'XMLHttpRequest',
+        'Token': '1282c9df85c4c78b6ea0b0341891ec89',
         'Sec-Ch-Ua-Platform': '"Windows"',
         'Origin': 'https://icp.chinaz.com',
         'Sec-Fetch-Site': 'same-origin',
         'Sec-Fetch-Mode': 'cors',
         'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://icp.chinaz.com',
-        # 'Accept-Encoding': 'gzip, deflate',
+        'Referer': 'https://icp.chinaz.com/qq.com',
+        # 'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Priority': 'u=1, i',
     }
-
+    # g了，留坑 Rd + Ts + Token
     json_data = {
         'keyword': target,
     }
 
     r = requests.post('https://icp.chinaz.com/index/api/queryPermit', headers=headers, json=json_data, verify=False)
     rj = r.json()
-    return [{"ICP备案号":rj.get('data')}, ]
+    return [{"ICP备案号":rj.get('data',rj.get('msg'))}, ]
 
 
 if __name__ == '__main__':
-    target = 'qq.com'
+    target = 'chinaz.com'
     print(execute(target))
